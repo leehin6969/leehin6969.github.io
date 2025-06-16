b640289963c841819bfdf982be8d0449819ff63f
JesseMAC <redmmo6969@gmail.com>
2025-06-16 13:04:24 -0600

    Updates

--- assets/Projects-BMF5LD2b.js
+++ assets/Projects-BMF5LD2b.js
@@ -1,4 +1,4 @@
-import{r,j as i,n as a,m as d}from"./index-C7r6k0E_.js";const s=a.section`
+import{r,j as i,n as a,m as d}from"./index-BnIn9Tkj.js";const s=a.section`
   display: flex;
   align-items: center;
   justify-content: flex-end;
@@ -139,4 +139,4 @@ import{r,j as i,n as a,m as d}from"./index-C7r6k0E_.js";const s=a.section`
   @media (max-width: 480px) {
     font-size: 0.75rem;
   }
-`,g={tap:{scale:.98}},w=r.memo(function(){const n=r.useMemo(()=>[{id:1,title:"MELL",date:"Dec. 2024",type:"Dev & Design",link:"https://mell.vercel.app/"},{id:2,title:"Transit-Go",date:"Nov. 2024",type:"Dev & Design",link:"https://transit-go-one.vercel.app/"},{id:3,title:"HKJC Calligraphy Metaverse Website",date:"Aug. 2024",type:"Dev / Design: Carol Mak",link:"https://jc-ccultureatt.zuni.org.hk/en/calligraphy/opening-3/"},{id:4,title:"MEBOARD",date:"Oct. 2023",type:"Dev & Design",link:"https://social-app-drab.vercel.app/"}],[]),o=t=>{t&&window.open(t,"_blank","noopener,noreferrer")};return i.jsx(s,{children:i.jsx(m,{children:i.jsx(l,{children:n.map(t=>i.jsxs(p,{variants:g,whileTap:"tap",onClick:()=>o(t.link),children:[i.jsx(c,{children:t.title}),i.jsxs(h,{children:[i.jsx("span",{children:t.date}),i.jsx("span",{children:t.type})]}),i.jsx(x,{children:t.description})]},t.id))})})})});export{w as default};
+`,g={tap:{scale:.98}},w=r.memo(function(){const n=r.useMemo(()=>[{id:1,title:"MELL",date:"Dec. 2024",type:"Dev & Design",link:"https://mell.vercel.app/"},{id:2,title:"Transit-Go (HK Ver.)",date:"Nov. 2024",type:"Dev & Design",link:"https://transit-go-one.vercel.app/"},{id:3,title:"HKJC Calligraphy Metaverse Website",date:"Aug. 2024",type:"Dev / Design: Carol Mak",link:"https://jc-ccultureatt.zuni.org.hk/en/calligraphy/opening-3/"},{id:4,title:"MEBOARD",date:"Oct. 2023",type:"Dev & Design",link:"https://social-app-drab.vercel.app/"}],[]),o=t=>{t&&window.open(t,"_blank","noopener,noreferrer")};return i.jsx(s,{children:i.jsx(m,{children:i.jsx(l,{children:n.map(t=>i.jsxs(p,{variants:g,whileTap:"tap",onClick:()=>o(t.link),children:[i.jsx(c,{children:t.title}),i.jsxs(h,{children:[i.jsx("span",{children:t.date}),i.jsx("span",{children:t.type})]}),i.jsx(x,{children:t.description})]},t.id))})})})});export{w as default};
